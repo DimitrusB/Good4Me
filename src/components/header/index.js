f95f74c8047d7a10096658e489fce04019eab7ce
DimitrusB <dimitrussboss@gmail.com>
2024-06-25 16:37:07 +0300

    fix

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -13,13 +13,13 @@ export const HeadMain = () => {
         </span>
         <span>Sign In / Register</span>
       </S.headerTop>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
             <img src={logo} alt="logo" />
           </a>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent"
@@ -27,45 +27,45 @@ export const HeadMain = () => {
             aria-expanded="false"
             aria-label="Переключатель навигации"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
                   HOME
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
+              <li className="nav-item">
+                <a className="nav-link" href="#">
                   SHOP
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link">FAQ'S</a>
+              <li className="nav-item">
+                <a className="nav-link">FAQ'S</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
+              <li className="nav-item">
+                <a className="nav-link" href="#">
                   STOCKISTS
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
+              <li className="nav-item">
+                <a className="nav-link" href="#">
                   WHOLESALE
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
+              <li className="nav-item">
+                <a className="nav-link" href="#">
                   CONTACT
                 </a>
               </li>
             </ul>
           </div>
-          <div class="container">
-            <a class="navbar-brand" href="#">
+          <div className="container">
+            <a className="navbar-brand" href="#">
               <img src={searchIcon} alt="search" />
             </a>
-            <a class="navbar-brand" href="#">
+            <a className="navbar-brand" href="#">
               <img src={recycleIcon} alt="rec" />
             </a>
           </div>
